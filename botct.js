23f3bddde03f0b3ecc2edf990a20020ec5069e5a
Zynx <106967034+Zyvnxx@users.noreply.github.com>
2026-01-06 20:56:26 +0700

    Add files via upload

--- botct.js
+++ botct.js
@@ -671,19 +671,5 @@ client.on('interactionCreate', async (interaction) => {
     }
 });
 
-
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Bot is running');
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Web server running on port ${PORT}`);
-});
-
-
 // Login bot dengan environment variable untuk keamanan
 client.login(process.env.DISCORD_TOKEN);
